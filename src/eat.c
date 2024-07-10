aa21e573a1355054f550fd2ae20040b7d579e49e
Tonton Twig <armand.larzilliere@epitech.eu>
2024-07-10 15:30:52 +0200

    Fixed lock order, changed routine a bit

--- src/eat.c
+++ src/eat.c
@@ -3,16 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   eat.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arlarzil <armand.larzilliere@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 19:28:39 by arlarzil          #+#    #+#             */
-/*   Updated: 2024/07/09 21:28:30 by arlarzil         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:30:11 by arlarzil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
 #include <stdio.h>
+
+static int	wait_death(t_philo *p)
+{
+	pthread_mutex_unlock(p->lfork);
+	while (1)
+	{
+		usleep(1000);
+		if (qmutex_get(&p->opt->run_m, &p->opt->run) == 0)
+			return (1);
+	}
+}
 
 static int	rl_eat(t_philo *p)
 {
@@ -21,6 +32,8 @@ static int	rl_eat(t_philo *p)
 	pthread_mutex_lock(&p->rfork);
 	if (action(p, "has taken a fork"))
 		return (pthread_mutex_unlock(&p->rfork), 1);
+	if (p->opt->p_count == 1)
+		return (wait_death(p));
 	pthread_mutex_lock(p->lfork);
 	if (action(p, "has taken a fork"))
 	{
@@ -80,17 +93,9 @@ void	test_eat_count(int n, t_philo *p)
 
 int	eat(t_philo *p, int *eat_count)
 {
-	if (p->num % 2)
-	{
-		usleep(100);
-		if (rl_eat(p))
-			return (1);
-	}
-	else
-	{
-		if (lr_eat(p))
-			return (1);
-	}
+	(void)(lr_eat);
+	if (rl_eat(p))
+		return (1);
 	*eat_count += 1;
 	test_eat_count(*eat_count, p);
 	return (0);
--- src/routine.c
+++ src/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arlarzil <armand.larzilliere@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:57:39 by arlarzil          #+#    #+#             */
-/*   Updated: 2024/07/09 21:13:08 by arlarzil         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:28:15 by arlarzil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@
 #include <stdio.h>
 #include <stdio.h>
 
-/*
-		if (opt->it != -1)
-		{
-			if (qmutex_get(&opt->count_m, &opt->rem_count) == opt->p_count)
-			{ // Check a faire faire par le philo
-				qmutex_set(&opt->run_m, &opt->run, 0);
-			}
-*/
 int	action(t_philo *self, const char *text)
 {
 	suseconds_t	stamp;
 
+	usleep(1000);
 	if (qmutex_get(&self->opt->run_m, &self->opt->run) == 0)
 		return (1);
 	stamp = get_timestamp(self->opt->start);
@@ -36,24 +29,11 @@ int	action(t_philo *self, const char *text)
 	return (0);
 }
 
-static int	wait_start(pthread_mutex_t *m, int *val)
-{
-	int	ret;
-
-	ret = 0;
-	while (!ret)
-	{
-		ret = qmutex_get(m, val);
-		usleep(10);
-	}
-	return (ret);
-}
-
 static int	philo_step(t_philo *p, int *eat_count)
 {
-	usleep(1000);
 	if (action(p, "is thinking"))
 		return (0);
+	usleep(2000);
 	if (eat(p, eat_count))
 		return (0);
 	if (action(p, "is sleeping"))
@@ -64,16 +44,12 @@ static int	philo_step(t_philo *p, int *eat_count)
 void	*routine(void *data)
 {
 	t_philo	*self;
-	int		flag = 1;
 	int		eat_count;
 
 	eat_count = 0;
 	self = data;
-	flag = wait_start(&self->opt->run_m, &self->opt->run);
-	if (self->num % 2)
-		usleep(8800);
-	if (flag == -1)
-		return (NULL);
+	if (self->num % 2 == 0)
+		usleep(1000);
 	while (philo_step(self, &eat_count))
 		;
 	return (NULL);
