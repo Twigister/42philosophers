83904ba21ae41d3e274e5ff734e8d394fa861ee4
Twigister <armand.larzilliere@gmail.com>
2024-08-24 11:37:41 +0200

    Added a more precise version of the sleep function (Useful for tighter timings

--- philo/src/utils/msleep.c
+++ philo/src/utils/msleep.c
@@ -1,6 +1,21 @@
 #include <unistd.h>
+#include <sys/time.h>
+
+static long	mstime(void)
+{
+	struct timeval	ini;
+
+	gettimeofday(&ini, 0);
+	return (ini.tv_sec * 1000 + ini.tv_usec / 1000);
+}
 
 void	msleep(int msec)
 {
-	usleep(msec * 1000);
+	long	goal;
+
+	goal = mstime() + msec;
+	while (goal > mstime())
+	{
+		usleep(50);
+	}
 }
